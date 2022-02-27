d20532acc4cd910b0f619cc4c21c9e6106da4c76
Steven Hecking <shecking1115@gmail.com>
2022-02-26 19:43:28 -0500

    committing completed q3

--- mysqlWeek5Assignment/src/optional/MineralOptional.java
+++ mysqlWeek5Assignment/src/optional/MineralOptional.java
@@ -1,43 +1,43 @@
 package optional;
 
-import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
-
 import week5Assignment.DRGMineral;
 
 public class MineralOptional {
 	
-	// getting the static list
-	static List<DRGMineral> minerals = DRGMineral.getMinerals();
-
-	// main method
+	// instance variable for exception message
+	public static NoSuchElementException e = new NoSuchElementException("Mineral was not found");
+	
 	public static void main(String[] args) {
-		// method call(s?) here
+		// call method B
+		callingFindMineral();
 	}
 	
-	// method A: a method that accepts Optional of object and returns unwrapped object
-	// template directly from assignment:
-	// public Cheese cheesyMethod(Optional<Cheese> optionalCheese) {...}
-	public static DRGMineral findMineral(Optional<DRGMineral> optionalMineral) {
-		// if present return unwrapped object
-		// take input via a scanner or search list of mineral objects above?
-		// where is the information coming from?
-		if (optionalMineral.isPresent()) {
-			// return unwrapped object
-			return optionalMineral.get();
-		// if not present throw Exception
-		} else {
-			throw new NoSuchElementException("mineral is not present");
-		}
+	// method A
+	// accept in an Optional object, return object if it exists
+	private static DRGMineral findMineral(Optional<DRGMineral> optionalMin) {
+		// throw a NSEE (**using orElseThrow**) with a message if no object
+		// use lambda with no parameters
+		// return type is DRGMineral
+		return optionalMin.orElseThrow(() -> e);
 	}
-
-	// method B (?)
-	// what is the difference between method A and B? Why are two methods needed?
-	public static void callingA(Optional<DRGMineral> optionalMineral) {
-		// Call A with an object wrapped by an Optional
-		// print the object
-		// Also call A with empty Optional
-		// print exception message
+	
+	// method B
+	// two separate method calls of A
+	// one with a new Optional object and one with empty optional
+	private static void callingFindMineral() {
+		// findMineral call #1
+		DRGMineral fancyMineral = findMineral(Optional.of(new DRGMineral("red sugar")));
+		// return not possible, creates unreachable code
+		System.out.println(fancyMineral);
+		
+		// try catch using e and an empty optional
+		try {
+			// findMineral call #2
+			findMineral(Optional.empty());
+		} catch (NoSuchElementException e){
+			System.out.println(e.getMessage());
+		}
 	}
 }
